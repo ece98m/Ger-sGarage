4eb3bdc70787cca3efe14ad5e5573a08e0005719
ece98m <gulcef.ece@gmail.com>
2023-07-19 23:47:06 +0100

    booking system

--- booking.php
+++ booking.php
@@ -1,6 +1,5 @@
 <?php include "header.php"; ?>
 
-
 <?php 
 session_start();
 
@@ -16,38 +15,11 @@ $successMessage = ""; // Başlangıçta boş bir başarı mesajı
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // İstenen form verilerini almak
-
-    $contactEmail = $_POST['contact_email'];
-
-    $vehicleType = $_POST['vehicle_type'];
-    $vehicleMake = $_POST['vehicle_make'];
-    $vehicleLicense = $_POST['vehicle_license'];
-    $vehicleEngineType = $_POST['vehicle_engine_type'];
-
+    $date = $_POST["date"];
     $serviceType = $_POST['service_type'];
     $service_description = $_POST['service_description'];
+    
 
-    // Müşteri ID'sini bulmak için sorguyu oluştur
-    $sql = "SELECT idcustomers FROM customers WHERE email='$contactEmail'";
-    $result = $mysqli->query($sql);
-
-    if ($result->num_rows > 0) {
-        // Müşteri bulundu, ID'yi al
-        $row = $result->fetch_assoc();
-        $customerId = $row['idcustomers']; //bu idyi gerekli değişkene ata 
-
-        // vehicles tablosuna yeni kayıt ekleme
-        $sql = "INSERT INTO vehicles (idcustomers, vehicle_type, make, license, engine_type)
-        VALUES ('$customerId', '$vehicleType', '$vehicleMake', '$vehicleLicense', '$vehicleEngineType')";
-        if ($mysqli->query($sql) === true) {
-            $vehicleId = $mysqli->insert_id; //eklenen aracın id sini booking tablosuna eklemek için çekme
-            echo "Yeni araç eklendi. Araç ID'si: " . $vehicleId;
-        } else {
-            echo "Araç ekleme hatasi: " . $mysqli->error;
-        }
-    } else {
-        echo "No customer registered with this email before.";
-    }
 
     // Servis ID'sini bulmak için sorguyu oluştur
     $sql = "SELECT service_id FROM services WHERE service_name='$serviceType'";
@@ -62,18 +34,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "NO SERVICE HAS BEEN FOUND.";
     }
 
-    $idMechanic = rand(1, 4);
-    $bookingDate = date("Y-m-d");
-    $status = "aaa";
-    $sql = "INSERT INTO bookings (idcustomers, idvehicles, booking_date, service_id, customer_note, id_mechanics, status)
-            VALUES ('$customerId', '$vehicleId', '$bookingDate' , '$serviceId', '$service_description', '$idMechanic', '$status' )";
+    $sql = "SELECT idcustomers FROM customers WHERE email='$email'";
+    $result = $mysqli->query($sql);
 
-    if ($mysqli->query($sql) === true) {
-        echo "Yeni rezervasyon başarıyla eklendi.";
-    } else {
-        echo "Rezervasyon ekleme hatası: " . $mysqli->error;
+   
+    if ($result->num_rows > 0) {
+      // Müşteri bulundu, ID'yi al
+      $row = $result->fetch_assoc();
+      $customerId = $row['idcustomers']; // Bu idyi gerekli değişkene ata
     }
-} 
+
+  
 
 // services tablosundan tüm satırları almak için sorguyu oluştur
 $sql = "SELECT service_id, service_name FROM services";
@@ -87,6 +58,28 @@ $sql = "SELECT idcustomers FROM customers WHERE email='$email'";
     $row = $result->fetch_assoc();
     $customerId = $row['idcustomers']; // Bu idyi gerekli değişkene ata
   }
+
+  $sql = "SELECT idvehicles FROM vehicles WHERE idcustomers=' $customerId'";
+  $result = $mysqli->query($sql);
+  
+  if ($result->num_rows > 0) {
+    // Müşteri bulundu, ID'yi al
+    $row = $result->fetch_assoc();
+    $vehicleId = $row['idvehicles']; // Bu idyi gerekli değişkene ata
+  } 
+  
+  
+  $idMechanic = rand(1, 4);
+  $status = "aaa";
+  $sql = "INSERT INTO bookings (idcustomers, idvehicles, booking_date, service_id, customer_note, id_mechanics, status)
+          VALUES ('$customerId', '$vehicleId', '$date' , '$serviceId', '$service_description', '$idMechanic', '$status' )";
+
+  if ($mysqli->query($sql) === true) {
+      echo "Yeni rezervasyon başarıyla eklendi.";
+  } else {
+      echo "Rezervasyon ekleme hatası: " . $mysqli->error;
+  }
+} 
 // services tablosundan tüm satırları almak için sorguyu oluştur
    
 $sql = "SELECT * FROM vehicles WHERE idcustomers='$customerId'";
@@ -96,20 +89,21 @@ $licenses = $mysqli->query($sql);
 
 
 
-<section id="booking">
-<head>
 
-    <link rel="stylesheet" type="text/css" href="stylebooking.css">
-</head>
+
+
+
+
+<section id="booking">
+    <head>
+        <link rel="stylesheet" type="text/css" href="stylebooking.css">
+    </head>
     <h2>Book Your Service or Repair</h2>
 
     <form action="" method="post">
-   
-
-     
         <label for="vehicle_type">Choose Your Vehicle </label>
         <select id="vehicle_type" name="vehicle_type" required>
-         <?php
+            <?php
             if ($licenses->num_rows > 0) {
                 while ($row = $licenses->fetch_assoc()) {
                     echo "<option value=\"" . $row['license'] . "\">" . $row['license'] . "</option>";
@@ -119,8 +113,6 @@ $licenses = $mysqli->query($sql);
             }
             ?>
         </select>
-
-
 
         <label for="service_type">Service Type:</label>
         <select id="service_type" name="service_type" required>
@@ -135,18 +127,39 @@ $licenses = $mysqli->query($sql);
             ?>
         </select>
 
+        <label for="date">Tarih:</label>
+        <input type="date" id="date" name="date" min="<?php echo date('Y-m-d'); ?>" max="<?php echo date('Y-m-d', strtotime('+1 year')); ?>" required>
+
+        <script>
+            document.getElementById("date").addEventListener("change", function() {
+                var selectedDate = new Date(this.value);
+                var dayOfWeek = selectedDate.getDay(); // 0 (Pazar) ile 6 (Cumartesi) arasında bir değer döndürür
+
+                if (dayOfWeek == 0) {
+                    this.value = ""; // Seçilen tarihi temizle
+                    alert("Oops! Ger's Garage is not open on Sundays");
+                }
+            });
+        </script>
+
         <label for="service_description">Service Description:</label>
         <textarea id="service_description" name="service_description"></textarea>
+<script>
+    document.querySelector("form").addEventListener("submit", function(event) {
+        var serviceDescription = document.getElementById("service_description").value;
 
+        if (serviceDescription.trim() === "") {
+            event.preventDefault(); // Formun gönderilmesini engelle
+            alert("Please fill up the description part");
+        }
+    });
+</script>
         <input type="submit" value="Submit">
     </form>
 </section>
 
 <!-- Diğer içerikler buraya eklenebilir -->
 
-
-
-
 <footer>
     <p>&copy; 2023 Ger's Garage. </p>
-</footer
+</footer>
