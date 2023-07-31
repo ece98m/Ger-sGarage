a27a8ff2bbc95ce79e864cbe79e009625c0eeb85
ece98m <gulcef.ece@gmail.com>
2023-07-31 11:49:45 +0100

    a

--- adminpage/admin.php
+++ adminpage/admin.php
@@ -15,11 +15,12 @@ if (!empty($_SESSION['username'])) {
 <body>
     <div class="container">
         <nav class="navbar"> <!-- Added the class "navbar" to the nav element -->
-            <h1>Welcome to Ger's Garage Admin Page</h1>
+            <h1>Ger's Garage Admin Page</h1>
             <div>
                 <a href="admincreate.php" class="btn nav-link">Add Admin</a> <!-- Added the class "nav-link" to the anchor elements -->
                 <a href="bookings.php" class="btn nav-link">Reservations</a>
                 <a href="staffroster.php" class="btn nav-link">Staff Roster</a>
+                <a href="vehicle_products.php" class="btn nav-link">Vehicle Products</a>
                 <a href="cost_allocation.php" class="btn nav-link">Allocate Costs</a>
                 <?php if (!empty($_SESSION['username'])) { ?>
                     <a href="adminlogout.php" class="btn btn-primary nav-link">Logout</a> <!-- Added the class "nav-link" to the anchor element -->
--- adminpage/get_mechanics.php
+++ adminpage/get_mechanics.php
@@ -15,31 +15,47 @@ if (isset($_POST['date'])) {
     foreach ($dates as $date) {
         $sql = "SELECT id_mechanics FROM bookings WHERE booking_date = '$date'";
         $result = mysqli_query($mysqli, $sql);
-        $mechanicNamesForTheDay = [];
+        $mechanicIdsForTheDay = [];
         if (mysqli_num_rows($result) > 0) {
             while($row = mysqli_fetch_assoc($result)) {
-                $mechanicId = $row["id_mechanics"];
-                $nameSql = "SELECT firstname, surname FROM mechanics WHERE id_mechanics = '$mechanicId'";
-                $nameResult = mysqli_query($mysqli, $nameSql);
-                if (mysqli_num_rows($nameResult) > 0) {
-                    while($nameRow = mysqli_fetch_assoc($nameResult)) {
-                        $mechanicNamesForTheDay[] = $nameRow["firstname"] . ' ' . $nameRow["surname"];
-                    }
-                }
+                $mechanicIdsForTheDay[] = $row["id_mechanics"];
             }
         }
-        $mechanicNames[$date] = $mechanicNamesForTheDay;
+        $mechanicNames[$date] = $mechanicIdsForTheDay;
     }
 
     echo '<table>';
-    foreach ($mechanicNames as $date => $names) {
+    echo '<tr><th>MECHANICS</th>';
+    foreach ($dates as $date) {
+        echo '<th>'.$date.'</th>';
+    }
+    echo '</tr>';
+
+    $allMechanics = [];
+    foreach ($mechanicNames as $date => $mechanicIds) {
+        foreach ($mechanicIds as $mechanicId) {
+            $nameSql = "SELECT firstname, surname FROM mechanics WHERE id_mechanics = '$mechanicId'";
+            $nameResult = mysqli_query($mysqli, $nameSql);
+            if (mysqli_num_rows($nameResult) > 0) {
+                while($nameRow = mysqli_fetch_assoc($nameResult)) {
+                    $allMechanics[$mechanicId]['name'] = $nameRow["firstname"] . ' ' . $nameRow["surname"];
+                    $allMechanics[$mechanicId]['dates'][$date] = 1;
+                }
+            }
+        }
+    }
+
+    foreach ($allMechanics as $mechanicId => $mechanicData) {
         echo '<tr>';
-        echo '<td>'.$date.'</td>';
-        foreach ($names as $name) {
-            echo '<td>'.$name.'</td>';
+        echo '<td>'.$mechanicData['name'].'</td>';
+        foreach ($dates as $date) {
+            echo '<td>';
+            echo isset($mechanicData['dates'][$date]) ? 'X' : '';
+            echo '</td>';
         }
         echo '</tr>';
     }
+
     echo '</table>';
 }
 ?>
