8981a3c3f3c8d7cb6d6edb982b2c515fe44a4952
ece98m <gulcef.ece@gmail.com>
2023-08-13 11:01:06 +0100

    login buton on all admin pages added

--- adminpage/staffroster.php
+++ adminpage/staffroster.php
@@ -121,12 +121,12 @@ require "../userpages/connection.php"; ?>
       </div>
      </div>
      <ul class="navbar-nav  justify-content-end">
-      <li class="nav-item d-flex align-items-center">
-       <a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
-        <i class="fa fa-user me-sm-1"></i>
-        <span class="d-sm-inline d-none">Sign In</span>
-       </a>
-      </li>
+     <li class="nav-item d-flex align-items-center">
+    <a href="logout.php" class="nav-link text-body font-weight-bold px-0">
+      <i class="fa fa-user me-sm-1"></i>
+      <span class="d-sm-inline d-none">LogOut</span>
+    </a>
+  </li>
       <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
        <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
         <div class="sidenav-toggler-inner">
